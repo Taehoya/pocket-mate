4df9b0a84ba37ef6b503607ce48eb0b67ce51ccf
Taehoya <cth6809@naver.com>
2023-08-26 12:00:33 -0500

    ci:add lint checker

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"flag"
 	"log"
+	"net/http"
 	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
 var (
@@ -16,9 +20,8 @@ func init() {
 		host = "localhost"
 	}
 	flag.StringVar(&host, "host", host, "override default hostname")
-
 	if port == "" {
-		port = "3000"
+		port = "8080"
 	}
 	flag.StringVar(&port, "port", port, "override default port")
 }
@@ -26,5 +29,13 @@ func init() {
 func main() {
 	log.Print("starting process")
 
-	flag.Parse()
+	router := gin.Default()
+	router.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "OK",
+			"time":    time.Now().Format(time.RFC3339),
+		})
+	})
+
+	router.Run(host + ":" + port)
 }
