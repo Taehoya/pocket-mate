c508af8a04088028f6e18ba71fdcfd3408a1e993
Taehoya <cth6809@naver.com>
2023-08-30 14:33:34 -0500

    feat: Create initial tables

--- server/migration/000001_create_trip_table.up.sql
+++ server/migration/000001_create_trip_table.up.sql
@@ -1,10 +1,13 @@
-CREATE TABLE IF NOT EXISTS `trip` (
-    `trip_idx` BIGINT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `trips` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(30),
     `budget` DECIMAL(13,4),
     `country_id` BIGINT,
     `description` VARCHAR(255),
     `start_date_time` DATETIME NOT NULL,
     `end_date_time` DATETIME NOT NULL,
-    PRIMARY KEY (trip_idx)
+    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `deleted_at` DATETIME DEFAULT NULL, 
+    PRIMARY KEY (`id`)
 )
