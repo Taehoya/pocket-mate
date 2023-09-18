b54327d4fd1aef315897fc2cd89e83f94be92dfb
Taehoya <cth6809@naver.com>
2023-09-18 14:34:24 -0500

    chore: revise project structure

---
+++ server/pkg/repositories/country/repository_test.go
@@ -0,0 +1,36 @@
+package repository
+
+import (
+	"testing"
+
+	"github.com/Taehoya/pocket-mate/pkg/entities"
+	"github.com/Taehoya/pocket-mate/pkg/utils/testdb"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetCountryAll(t *testing.T) {
+	db, err := testdb.InitDB()
+	assert.NoError(t, err)
+
+	defer db.Close()
+
+	repository := NewCountryRepository(db)
+
+	t.Run("successfully get list of countries", func(t *testing.T) {
+		defer testdb.SetUp(db, "./tear_down.sql")
+		testdb.SetUp(db, "./tear_down.sql")
+		testdb.SetUp(db, "./setup.sql")
+
+		expected := []entities.Country{
+			{ID: 1, Code: "AF", Name: "Afghanistan", Currency: "؋"},
+			{ID: 2, Code: "AX", Name: "Aland Islands", Currency: "€"},
+			{ID: 3, Code: "AL", Name: "Albania", Currency: "Lek"},
+		}
+
+		actual, err := repository.GetCountryAll()
+
+		assert.NoError(t, err)
+		assert.Equal(t, actual, expected)
+	})
+
+}
---
+++ server/pkg/repositories/country/setup.sql
@@ -0,0 +1,4 @@
+INSERT INTO test.countries (id, code, name, currency) VALUES 
+    (1, 'AF', 'Afghanistan', '؋'),
+    (2, 'AX', 'Aland Islands', '€'),
+    (3, 'AL', 'Albania', 'Lek');
