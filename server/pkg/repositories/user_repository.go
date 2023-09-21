087dde17682f1759f803acdd1552eb9df7aea257
Taehoya <cth6809@naver.com>
2023-09-21 17:31:29 -0500

    update repositores package structure

---
+++ server/pkg/repositories/user_repository.go
@@ -0,0 +1,24 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/Taehoya/pocket-mate/pkg/entities"
+)
+
+type UserRepository struct {
+	db *sql.DB
+}
+
+func NewUserRepository(db *sql.DB) *UserRepository {
+	return &UserRepository{
+		db: db,
+	}
+}
+
+func (r *UserRepository) FindByID(ctx context.Context, id int64) (entities.User, error) {
+	User := entities.NewUser(1, "test", "123", "123")
+
+	return *User, nil
+}
