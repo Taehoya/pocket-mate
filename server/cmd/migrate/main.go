77d0c7bf571d593e8a2287d08d7abca9bd2eb044
Taehoya <cth6809@naver.com>
2023-09-22 00:07:28 -0500

    update to use environment value

--- server/cmd/migrate/main.go
+++ server/cmd/migrate/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/go-sql-driver/mysql"
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/mysql"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
@@ -15,7 +18,17 @@ func main() {
 	flag.StringVar(&direction, "direction", "up", "migrate cmd: up or down")
 	flag.Parse()
 
-	migrate, err := migrate.New("file://migration", "mysql://root:root@tcp(localhost:3306)/test")
+	dbConfig := mysql.Config{
+		User:      os.Getenv("DB_USER"),
+		Passwd:    os.Getenv("DB_PASSWORD"),
+		Net:       "tcp",
+		Addr:      fmt.Sprintf("%s:%s", os.Getenv("DB_HOST"), os.Getenv("DB_PORT")),
+		DBName:    os.Getenv("DB_NAME"),
+		ParseTime: true,
+	}
+
+	databaseURL := dbConfig.FormatDSN()
+	migrate, err := migrate.New("file://migration", fmt.Sprintf("mysql://%s", databaseURL))
 
 	if err != nil {
 		log.Printf("failed to init migration")
