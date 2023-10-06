aeff908644975f782fd241a6dab9371f0bb5a590
Taehoya <cth6809@naver.com>
2023-10-05 22:24:43 -0500

    implement account repository

--- server/pkg/entities/account.go
+++ server/pkg/entities/account.go
@@ -1,23 +1,39 @@
 package entities
 
-import "time"
-
 type Account struct {
 	id             int
 	userId         int
+	category       string
 	identification string
 	password       string
-	createdAt      time.Time
-	updatedAt      time.Time
 }
 
-func New(id int, userId int, identification string, password string, createdAt time.Time, updatedAt time.Time) *Account {
+func New(id int, userId int, category string, identification string, password string) *Account {
 	return &Account{
 		id:             id,
 		userId:         userId,
+		category:       category,
 		identification: identification,
 		password:       password,
-		createdAt:      createdAt,
-		updatedAt:      updatedAt,
 	}
+}
+
+func (a *Account) ID() int {
+	return a.id
+}
+
+func (a *Account) Category() string {
+	return a.category
+}
+
+func (a *Account) UserID() int {
+	return a.userId
+}
+
+func (a *Account) Identification() string {
+	return a.identification
+}
+
+func (a *Account) Password() string {
+	return a.password
 }
