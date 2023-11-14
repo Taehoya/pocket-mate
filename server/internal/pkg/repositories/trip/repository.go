84484c50b966b85cd79169b6d531d490ec0de99c
Taehoya <cth6809@naver.com>
2023-11-13 21:32:16 -0600

    implement trip repository layer

--- server/internal/pkg/repositories/trip/repository.go
+++ server/internal/pkg/repositories/trip/repository.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/Taehoya/pocket-mate/internal/pkg/entities"
 )
@@ -19,34 +20,7 @@ func NewTripRepository(db *sql.DB) *TripRepository {
 	}
 }
 
-func (r *TripRepository) GetTripAll() ([]*entities.Trip, error) {
-	var trips []*entities.Trip
-
-	rows, err := r.db.Query("SELECT * FROM pm.trip;")
-	if err != nil {
-		log.Printf("failed to execute query: %v\n", err)
-		return nil, fmt.Errorf("internal server error")
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var trip *entities.Trip
-		if err := rows.Scan(&trip.ID, &trip.Name, &trip.Budget, &trip.CountryId, &trip.Description, &trip.StartDateTime, &trip.EndDateTime); err != nil {
-			log.Printf("failed to scan row: %v\n", err)
-			return nil, fmt.Errorf("internal server error")
-		}
-		trips = append(trips, trip)
-	}
-
-	if err := rows.Err(); err != nil {
-		log.Printf("failed to scanning rows: %v\n", err)
-		return nil, fmt.Errorf("internal server error")
-	}
-
-	return trips, nil
-}
-
-func (r *TripRepository) SaveTrip(ctx context.Context, name string, userId int, budget float64, countryId int, description string, startDateTime string, endDateTime string) error {
+func (r *TripRepository) SaveTrip(ctx context.Context, name string, userId int, budget float64, countryId int, description string, startDateTime time.Time, endDateTime time.Time) error {
 	query := `
 		INSERT INTO trips
 			(name, user_id, budget, country_id, description, start_date_time, end_date_time)
@@ -72,4 +46,132 @@ func (r *TripRepository) SaveTrip(ctx context.Context, name string, userId int, 
 	}
 
 	return nil
+}
+
+func (r *TripRepository) GetTrip(ctx context.Context, userId int) ([]*entities.Trip, error) {
+	var trips []*entities.Trip
+	query := `
+		SELECT
+			id, name, budget, country_id, description, start_date_time, end_date_time, created_at, updated_at
+		FROM 
+			trips 
+		WHERE user_id = ?`
+
+	rows, err := r.db.QueryContext(ctx, query, userId)
+	if err != nil {
+		log.Printf("failed to execute query: %v\n", err)
+		return nil, fmt.Errorf("internal server error")
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id int
+		var name string
+		var budget float64
+		var countryId int
+		var description string
+		var startDateTime time.Time
+		var endDateTime time.Time
+		var createdAt time.Time
+		var updatedAt time.Time
+
+		if err := rows.Scan(&id, &name, &budget, &countryId, &description, &startDateTime, &endDateTime, &createdAt, &updatedAt); err != nil {
+			log.Printf("failed to scan trip: %v\n", err)
+			return nil, fmt.Errorf("internal server error")
+		}
+
+		trip := entities.NewTrip(id, name, budget, countryId, description, startDateTime, endDateTime, createdAt, updatedAt)
+		trips = append(trips, trip)
+	}
+
+	if err := rows.Err(); err != nil {
+		log.Printf("failed to iterate: %v\n", err)
+		return nil, fmt.Errorf("internal server error")
+	}
+
+	return trips, nil
+}
+
+func (r *TripRepository) DeleteTrip(ctx context.Context, tripId int) error {
+	query := `DELETE FROM trips WHERE id = ?;`
+
+	result, err := r.db.ExecContext(ctx, query, tripId)
+	if err != nil {
+		log.Printf("failed to execute query: %v\n", err)
+		return fmt.Errorf("internal Server Error")
+	}
+
+	rows, err := result.RowsAffected()
+	if err != nil {
+		log.Printf("failed to get affected rows: %\nv", err)
+		return fmt.Errorf("internal Server Error")
+	}
+
+	if rows != 1 {
+		log.Printf("expected 1 affected row, got %d\n", rows)
+		return fmt.Errorf("internal Server Error")
+	}
+
+	return nil
+}
+
+func (r *TripRepository) UpdateTrip(ctx context.Context, tripId int, name string, budget float64, countryId int, description string, startDateTime time.Time, endDateTime time.Time) error {
+	query := `UPDATE trips SET name = ?, budget = ?, country_id = ?, description = ?, start_date_time = ?, end_date_time = ? WHERE id = ?;`
+
+	result, err := r.db.ExecContext(ctx, query, name, budget, countryId, description, startDateTime, endDateTime, tripId)
+	if err != nil {
+		log.Printf("failed to execute query: %v\n", err)
+		return fmt.Errorf("internal Server Error")
+	}
+
+	rows, err := result.RowsAffected()
+	if err != nil {
+		log.Printf("failed to get affected rows: %\nv", err)
+		return fmt.Errorf("internal Server Error")
+	}
+
+	if rows != 1 {
+		log.Printf("expected 1 affected row, got %d\n", rows)
+		return fmt.Errorf("internal Server Error")
+	}
+
+	return nil
+}
+
+func (r *TripRepository) GetTripById(ctx context.Context, tripId int) (*entities.Trip, error) {
+	var trip *entities.Trip
+	query := `SELECT id, name, budget, country_id, description, start_date_time, end_date_time, created_at, updated_at FROM trips WHERE id = ?;`
+	rows, err := r.db.QueryContext(ctx, query, tripId)
+
+	if err != nil {
+		log.Printf("failed to execute query: %v\n", err)
+		return nil, fmt.Errorf("internal server error")
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id int
+		var name string
+		var budget float64
+		var countryId int
+		var description string
+		var startDateTime time.Time
+		var endDateTime time.Time
+		var createdAt time.Time
+		var updatedAt time.Time
+
+		if err := rows.Scan(&id, &name, &budget, &countryId, &description, &startDateTime, &endDateTime, &createdAt, &updatedAt); err != nil {
+			log.Printf("failed to scan trip: %v\n", err)
+			return nil, fmt.Errorf("internal server error")
+		}
+
+		trip = entities.NewTrip(id, name, budget, countryId, description, startDateTime, endDateTime, createdAt, updatedAt)
+	}
+
+	if err := rows.Err(); err != nil {
+		log.Printf("failed to iterate: %v\n", err)
+		return nil, fmt.Errorf("internal server error")
+	}
+
+	return trip, nil
 }
--- server/internal/pkg/repositories/trip/repository_test.go
+++ server/internal/pkg/repositories/trip/repository_test.go
@@ -3,8 +3,10 @@ package repository
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/Taehoya/go-utils/mysqltest"
+	"github.com/Taehoya/pocket-mate/internal/pkg/entities"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -26,10 +28,92 @@ func TestSaveTrip(t *testing.T) {
 		budget := 1000.0
 		countryId := 1
 		description := "test-description"
-		startDateTime := "2023-10-31 00:00:00"
-		endDateTime := "2023-10-31 00:00:00"
+		startDateTime := time.Now()
+		endDateTime := time.Now()
 
 		err := repository.SaveTrip(ctx, name, userId, budget, countryId, description, startDateTime, endDateTime)
 		assert.NoError(t, err)
+	})
+}
+
+func TestGetTrip(t *testing.T) {
+	db, err := mysqltest.InitDB()
+	assert.NoError(t, err)
+	defer db.Close()
+
+	repository := NewTripRepository(db)
+
+	t.Run("Successfully get trip", func(t *testing.T) {
+		defer mysqltest.SetUp(db, "./teardown_test.sql")
+		mysqltest.SetUp(db, "./teardown_test.sql")
+		mysqltest.SetUp(db, "./setup_test.sql")
+
+		ctx := context.TODO()
+		userId := 1
+
+		expected := []*entities.Trip{
+			entities.NewTrip(1, "test-name", 1, 1.0000, "test-description", time.Now(), time.Now(), time.Now(), time.Now()),
+		}
+
+		trips, err := repository.GetTrip(ctx, userId)
+		assert.NoError(t, err)
+		assert.NotNil(t, trips)
+		assert.Equal(t, trips[0].ID(), expected[0].ID())
+	})
+}
+
+func TestDeleteTrip(t *testing.T) {
+	db, err := mysqltest.InitDB()
+	assert.NoError(t, err)
+	defer db.Close()
+
+	repository := NewTripRepository(db)
+
+	t.Run("Successfully delete trip", func(t *testing.T) {
+		defer mysqltest.SetUp(db, "./teardown_test.sql")
+		mysqltest.SetUp(db, "./teardown_test.sql")
+		mysqltest.SetUp(db, "./setup_test.sql")
+
+		ctx := context.TODO()
+		tripId := 1
+
+		err := repository.DeleteTrip(ctx, tripId)
+		assert.NoError(t, err)
+
+		trip, err := repository.GetTripById(ctx, tripId)
+		assert.NoError(t, err)
+		assert.Nil(t, trip)
+	})
+}
+
+func TestUpdateTrip(t *testing.T) {
+	db, err := mysqltest.InitDB()
+	assert.NoError(t, err)
+	defer db.Close()
+
+	repository := NewTripRepository(db)
+
+	t.Run("successfully update trip", func(t *testing.T) {
+		defer mysqltest.SetUp(db, "./teardown_test.sql")
+		mysqltest.SetUp(db, "./teardown_test.sql")
+		mysqltest.SetUp(db, "./setup_test.sql")
+
+		ctx := context.TODO()
+		tripId := 1
+		userId := 1
+		name := "updated-name"
+		budget := 1000.0
+		countryId := 1
+		description := "updated-description"
+		startDateTime := time.Now()
+		endDateTime := time.Now()
+
+		err := repository.UpdateTrip(ctx, userId, name, budget, countryId, description, startDateTime, endDateTime)
+		assert.NoError(t, err)
+
+		trip, err := repository.GetTripById(ctx, tripId)
+		assert.NoError(t, err)
+		assert.NotNil(t, trip)
+		assert.Equal(t, trip.Name(), name)
 	})
 }
